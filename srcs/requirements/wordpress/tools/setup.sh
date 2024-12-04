eda87c0cf60100ae8422dff76cb5876a38e0756f
soufiane <soufianeajaite2@gmail.com>
2024-12-04 23:53:45 +0100

    wordpress setup done

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/html/wordpress
+
+mkdir -p /run/php
+
+sed -i 's/;clear_env = no/clear_env = no/g' /etc/php83/php-fpm.d/www.conf
+
+wp core download --allow-root --locale=en_US 
+
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+wp redis enable --allow-root
+
+wp plugin update --all --allow-root
+
+chown -R www-data:www-data /var/www/html/wordpress
+
+/usr/sbin/php-fpm83
